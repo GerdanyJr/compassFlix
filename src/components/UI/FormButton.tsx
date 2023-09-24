992d06864956c91ca313db8f4725c4e6fc5ca28a
Gerdany Junior <Asa_lindo@hotmail.com>
2023-09-24 20:33:27 -0300

    feat: :sparkles: add loading state to form button

--- src/components/UI/FormButton.tsx
+++ src/components/UI/FormButton.tsx
@@ -1,15 +1,18 @@
 import React from "react";
-import { Pressable, StyleSheet, Text } from "react-native";
+import { ActivityIndicator, Pressable, StyleSheet, Text } from "react-native";
 
 interface FormButtonProps {
     title: string,
+    isLoading?: boolean,
     onPress: () => void
 }
 
 export function FormButton(props: FormButtonProps): JSX.Element {
     return (
         <Pressable style={styles.button} onPress={props.onPress}>
-            <Text style={styles.title}>{props.title}</Text>
+            {props.isLoading ?
+                <ActivityIndicator color='black' size={20} /> :
+                <Text style={styles.title}>{props.title}</Text>}
         </Pressable>
     )
 }
@@ -17,13 +20,13 @@ export function FormButton(props: FormButtonProps): JSX.Element {
 const styles = StyleSheet.create({
     button: {
         backgroundColor: '#E9A6A6',
-        borderRadius: 30
+        borderRadius: 30,
+        paddingHorizontal: 20,
+        paddingVertical: 8,
     },
     title: {
         color: '#1f1d36',
         fontWeight: 'bold',
         fontSize: 16,
-        paddingHorizontal: 20,
-        paddingVertical: 8,
     }
 });
