db23f98cddefc9fe119a92f876ddd5bf465a49bd
Gerdany Junior <Asa_lindo@hotmail.com>
2023-09-26 15:15:42 -0300

    feat: :sparkles: create the movies fetch logic

--- src/screens/Home/MoviesScroll.tsx
+++ src/screens/Home/MoviesScroll.tsx
@@ -1,27 +1,48 @@
-import React from "react";
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, FlatList, StyleSheet, Text, View } from "react-native";
 import { MoviePoster } from "./MoviePoster";
+import { getMovies } from "../../services/moviesHttp";
 import { ShowcaseMovie } from "../../types/ShowcaseMovie";
 
 interface MoviesScrollProps {
     title: string,
-    data: ShowcaseMovie[],
-    onEndReached?: () => void
+    requestUrl: string
 }
 
 export function MoviesScroll(props: MoviesScrollProps) {
+    const [movies, setMovies] = useState<ShowcaseMovie[]>([]);
+    const [page, setPage] = useState<number>(1);
+    const [isLoading, setIsLoading] = useState<Boolean>(false);
+
+    useEffect(() => {
+        if (!isLoading) {
+            (async () => {
+                await loadMovies();
+            })();
+        }
+    }, []);
+
+    async function loadMovies() {
+        setIsLoading(true);
+        const fetchedMovies = await getMovies(props.requestUrl, page);
+
+        setPage(prevState => prevState + 1);
+        setMovies((prevState) => [...prevState, ...fetchedMovies]);
+        setIsLoading(false);
+    }
+
     return (
         <View style={styles.container}>
             <Text style={styles.title} numberOfLines={1} ellipsizeMode="tail">{props.title}</Text>
             <FlatList
-                data={props.data}
+                data={movies}
                 keyExtractor={(item) => item.id.toString()}
                 renderItem={({ item }) => <MoviePoster id={item.id} url={item.poster_path} />}
                 ItemSeparatorComponent={() => <View style={{ paddingHorizontal: 4 }}></View>}
-                horizontal
                 showsHorizontalScrollIndicator={false}
                 onEndReachedThreshold={0.9}
-                onEndReached={props.onEndReached}
+                onEndReached={loadMovies}
+                horizontal
             />
         </View>
     )
